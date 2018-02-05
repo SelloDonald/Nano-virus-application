0986605ed09679add121e25b8a6c4db6326d1cbc
SelloDonald <36155584+SelloDonald@users.noreply.github.com>
2018-02-05 14:10:59 +0200

    Add files via upload

---
+++ Cell.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NanoVirusApplication
+{
+    public class Cell
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public CellType Type { get; set; }
+
+        public List<Cell> GetCells()
+        {
+            List<Cell> cells = new List<Cell>();
+            Random randomNumber = new Random();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i <= 5)
+                {
+                    var cell = new Cell
+                    {
+                        Type = CellType.TumorousCell,
+                        X = randomNumber.Next(1, 5001),
+                        Y = randomNumber.Next(1, 5001),
+                        Z = randomNumber.Next(1, 5001)
+                    };
+
+                    cells.Add(cell);
+                }
+
+                if (i > 5 && i <= 30)
+                {
+                    var cell = new Cell
+                    {
+                        Type = CellType.WhiteBloodCell,
+                        X = randomNumber.Next(1, 5001),
+                        Y = randomNumber.Next(1, 5001),
+                        Z = randomNumber.Next(1, 5001)
+                    };
+
+                    cells.Add(cell);
+                }
+
+                if (i > 30)
+                {
+                    var cell = new Cell
+                    {
+                        Type = CellType.RedBloodCell,
+                        X = randomNumber.Next(1, 5001),
+                        Y = randomNumber.Next(1, 5001),
+                        Z = randomNumber.Next(1, 5001)
+                    };
+
+                    cells.Add(cell);
+                }
+            }
+
+            return cells;
+        }
+
+        public Cell GetBloodCell(List<Cell> cells, CellType cellType)
+        {
+            var randomNumber = new Random();
+            var redBloodCells = cells.Where(c => c.Type == cellType);
+            var cellIndex = randomNumber.Next(redBloodCells.Count());
+            return redBloodCells.ElementAt(cellIndex);
+        }
+
+        public double CalculateDistanceBetweenCells(Cell firstCell, Cell secondCell)
+        {
+            var value = ((firstCell.X - secondCell.X) * (firstCell.X - secondCell.X)) +
+                       ((firstCell.Y - secondCell.Y) * (firstCell.Y - secondCell.Y)) +
+                       ((firstCell.Z - secondCell.Z) * (firstCell.Z - secondCell.Z));
+
+            return Math.Sqrt(value);
+        }
+
+        //https://docs.microsoft.com/en-us/dotnet/standard/io/how-to-write-to-a-text-file
+        public void WriteToTextFile(string message)
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            using (StreamWriter outputFile = new StreamWriter(path + @"\NanoVirusStatus.txt", true))
+            {
+                outputFile.WriteLine(message);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NanoVirusApplication
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var cell = new Cell();
+            var cells = cell.GetCells();
+            var distance = 0.0;
+            var cycle = 0;
+            var isInfected = false;
+            var message = string.Empty;
+            var bloodCell = new Cell();
+
+            Console.WriteLine("Nano Virus Application in progress, please wait...");
+            Console.WriteLine("Writing to a file in \"My Documents\" Folder...");
+
+            foreach (var _cell in cells)
+            {
+                while (cycle < 5)
+                {
+
+                    if (cells.Where(c => c.Type == CellType.RedBloodCell).Count() > 0)
+                    {
+                        bloodCell = cell.GetBloodCell(cells, CellType.RedBloodCell);
+                    }
+                    else if (cells.Where(c => c.Type == CellType.WhiteBloodCell).Count() > 0)
+                    {
+                        bloodCell = cell.GetBloodCell(cells, CellType.WhiteBloodCell);
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    if (!isInfected)
+                    {
+                        foreach (var tumorous in cells.Where(c => c.Type == CellType.TumorousCell))
+                        {
+
+                            distance = cell.CalculateDistanceBetweenCells(bloodCell, tumorous);
+
+                            if (distance <= 5000)
+                            {
+                                if (bloodCell.Type == CellType.TumorousCell)
+                                {
+                                    //TODO: The remove of Tumorous cell from the list
+                                    // cells.Remove(bloodCell);
+                                }
+                                else
+                                {
+                                    bloodCell.Type = CellType.TumorousCell;
+                                }
+
+                                isInfected = true;
+                            }
+
+                            if (cells.Where(c => c.Type == CellType.TumorousCell).Count() == 0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    message = "Tumorous cells = " + cells.Where(c => c.Type == CellType.TumorousCell).Count() + "\n Red Blood Cells = " + cells.Where(c => c.Type == CellType.RedBloodCell).Count() + "\n White Blood Cells = " + cells.Where(c => c.Type == CellType.WhiteBloodCell).Count();
+
+                    cell.WriteToTextFile(message);
+
+                    ++cycle;
+
+                    if (cycle == 5)
+                    {
+                        cycle = 0;
+                        message = "\n";
+                        isInfected = false;
+                    }
+                }
+
+            }
+
+            Console.WriteLine("...End of Nano Virus Application...");
+            Console.ReadLine();
+        }
+    }
+}
